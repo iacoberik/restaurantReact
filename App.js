b237fce117cecea65091097a8971bcd3bef504a1
iacoberik <iacob.erik320@gmail.com>
2023-12-27 00:37:05 +0200

    first ep

---
+++ App.js
@@ -0,0 +1,21 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "heading" }, "I`m h1 tag"),
+    React.createElement("h2", { id: "heading-two" }, "I`m h2 tag"),
+  ]),
+  React.createElement("div", { id: "child-two" }, [
+    React.createElement("h1", { id: "heading" }, "I`m h1 tag"),
+    React.createElement("h2", { id: "heading-two" }, "I`m h2 tag"),
+  ]),
+]);
+
+console.log(parent);
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World from REACT!"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
