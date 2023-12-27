2d0389238f533a1f85c63720e8eecf0ab561fa46
iacoberik <iacob.erik320@gmail.com>
2023-12-27 12:28:58 +0200

    Episode 2

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", { id: "heading" }, "I`m h1 tag"),
@@ -9,7 +12,7 @@ const parent = React.createElement("div", { id: "parent" }, [
   ]),
 ]);
 
-console.log(parent);
+console.log(parent); // parent variable is an Object that react converts into html
 
 const heading = React.createElement(
   "h1",
